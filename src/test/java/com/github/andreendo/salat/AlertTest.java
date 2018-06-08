139214cabe3f57fde49a4897882c6ba3c8071e06
renilsonpicinin <renilsonpicinin@gmail.com>
2018-06-07 21:06:43 -0300

    Teste de Alert
    Identificar e tratar Alert

---
+++ src/test/java/com/github/andreendo/salat/AlertTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.andreendo.salat;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author renil
+ */
+public class AlertTest {
+    
+    private WebDriver webDriver;
+    
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\renil\\Downloads\\chromedriver.exe");
+    }
+    
+    @Before
+    public void before() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);   
+        webDriver.manage().window().maximize();
+    }
+    
+    @After
+    public void after() {
+        webDriver.close();
+    }
+    
+    @Test
+    public void test01(){
+        Driver driver = new WebAppDriver(webDriver, "https://www.crunchyroll.com/", "crunchyroll.com");
+        driver.restart();
+        List<FireableEvent> events = driver.getCurrentFireableEvents();
+        
+        StopCondition stopCondition = new CounterStopCondition(100);
+        Tester tester = new Tester(driver, stopCondition, new Random());
+        tester.executeRandomTest();
+    }
+}
