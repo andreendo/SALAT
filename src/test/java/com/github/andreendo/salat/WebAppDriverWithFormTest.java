16641c104edb3ad2b302643660d8f87f05ba053c
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-06-15 15:42:00 -0300

    finalizando testes

---
+++ src/test/java/com/github/andreendo/salat/WebAppDriverWithFormTest.java
@@ -0,0 +1,50 @@
+package com.github.andreendo.salat;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author fabriciojso
+ */
+public class WebAppDriverWithFormTest {
+    
+    private WebDriver webDriver;
+    
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "/Users/fabriciojso/utfpr/topicos-avancados-em-teste/jars/chromedriver");
+    }
+    
+    @Before
+    public void before() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);   
+        webDriver.manage().window().maximize();
+    }
+    
+    @After
+    public void after() {
+        webDriver.close();
+    }  
+    
+     @Test
+    public void test01() {
+         Driver driver = new WebAppDriverWithForm(webDriver, "https://www.facebook.com/", "facebook.com");       
+        
+        StopCondition stopCondition = new CounterStopCondition(200);
+        Tester tester = new Tester(driver, stopCondition, new Random());
+        tester.executeRandomTest();
+    }  
+}
